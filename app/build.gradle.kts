29796d2c623e03a6ebed50bc6440fd0373b341ef
Serkan <serkan@basefy.com>
2021-10-30 22:36:21 +0300

    Updated app gradle file

---
+++ app/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-kapt")
+}
+
+
+android {
+    compileSdk = Configs.compileSdk
+    defaultConfig{
+        applicationId = Configs.applicationId
+        versionCode = Configs.versionCode
+        versionName = Configs.versionName
+        minSdk = Configs.minSdk
+        targetSdk = Configs.targetSdk
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = Options.sourceCompatibility
+        targetCompatibility = Options.targetCompatibility
+    }
+    kotlinOptions {
+        jvmTarget = Options.jvmTarget
+    }
+    viewBinding{
+        isEnabled = true
+    }
+
+
+}
+
+dependencies {
+    implementation(Libs.coreKtx)
+    implementation(Libs.hilt)
+    implementation(Libs.appCompat)
+    implementation(Libs.material)
+    implementation(Libs.constaint)
+    implementation(Libs.navigationFragment)
+    implementation(Libs.navigationUI)
+    testImplementation(Libs.junit)
+    androidTestImplementation(Libs.junit_ext)
+    androidTestImplementation(Libs.espresso)
+    kapt(Libs.hiltCompiler)
+}
--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -7,6 +7,14 @@ object Versions {
     const val moshi = "1.12.0"
     const val hilt = "2.40"
     const val build = "7.0.3"
+    const val appCompat = "1.3.1"
+    const val material = "1.4.0"
+    const val constraint = "2.1.1"
+    const val navigation_fragment = "2.3.5"
+    const val navigation_ui = "2.3.5"
+    const val junit = "4.+"
+    const val junit_ext = "1.1.3"
+    const val espresso = "3.4.0"
 }
 
 object Libs {
@@ -16,12 +24,23 @@ object Libs {
     val hilt by lazy { "com.google.dagger:hilt-android:${Versions.hilt}" }
     val hiltCompiler by lazy { "com.google.dagger:hilt-compiler:${Versions.hilt}" }
     val moshiConverter by lazy { "com.squareup.retrofit2:converter-moshi:${Versions.retrofit}" }
+    val appCompat by lazy { "androidx.appcompat:appcompat:${Versions.appCompat}"}
+    val material by lazy { "com.google.android.material:material:${Versions.material}"}
+    val constaint by lazy { "androidx.constraintlayout:constraintlayout:${Versions.constraint}"}
+    val navigationFragment by lazy { "androidx.navigation:navigation-fragment-ktx:${Versions.navigation_fragment}"}
+    val navigationUI by lazy { "androidx.navigation:navigation-ui-ktx:${Versions.navigation_ui}"}
+    val junit by lazy { "junit:junit:${Versions.junit}"}
+    val junit_ext by lazy { "androidx.test.ext:junit:${Versions.junit_ext}"}
+    val espresso by lazy { "androidx.test.espresso:espresso-core:${Versions.espresso}"}
 }
 
 object Configs {
     const val compileSdk = 31
     const val minSdk = 21
     const val targetSdk = 31
+    const val applicationId = "com.mavra.nasaassignment"
+    const val versionCode = 1
+    const val versionName = "1.0.0"
 }
 
 object ClassPlugins {
