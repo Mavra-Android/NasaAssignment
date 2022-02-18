2fd6ef5a98313bee64b95e031cebd3f548e85682
Serkan Ozaydın <serkanozaydin@teknasyon.com>
2022-02-18 03:35:59 +0300

    Created list screen

--- core/src/main/java/com/mavra/core/components/custom_views/ToolbarComponent.kt
+++ core/src/main/java/com/mavra/core/components/custom_views/ToolbarComponent.kt
@@ -1,11 +1,14 @@
 package com.mavra.core.components.custom_views
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.LinearLayout
+import com.mavra.core.R
 import com.mavra.core.databinding.CoreToolbarBinding
 
+@SuppressLint("Recycle", "CustomViewStyleable")
 class ToolbarComponent @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null
@@ -15,4 +18,10 @@ class ToolbarComponent @JvmOverloads constructor(
     private var binding: CoreToolbarBinding =
         CoreToolbarBinding.inflate(LayoutInflater.from(context), this, true)
 
+    init {
+        val attributes = context.obtainStyledAttributes(attrs, R.styleable.CustomToolbar)
+        attributes.apply {
+            binding.toolbar.title = getString(R.styleable.CustomToolbar_android_text)
+        }
+    }
 }
