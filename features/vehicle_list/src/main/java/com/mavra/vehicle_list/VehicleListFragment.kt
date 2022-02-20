ddaa556cd8b31e354e372c9cfeb3a39cbdd64d94
Serkan Ozaydın <serkanozaydin@teknasyon.com>
2022-02-20 04:05:59 +0300

    Updated rover list codes

--- features/vehicle_list/src/main/java/com/mavra/vehicle_list/VehicleListFragment.kt
+++ features/vehicle_list/src/main/java/com/mavra/vehicle_list/VehicleListFragment.kt
@@ -9,10 +9,13 @@ import androidx.navigation.fragment.findNavController
 import com.mavra.core.utility.attach
 import com.mavra.core.utility.linearLayoutManagerVertical
 import com.mavra.core.view.BaseFragment
+import com.mavra.domain.model.DomainPhoto
 import com.mavra.vehicle_list.databinding.FragmentVehicleListBinding
 import com.mavra.vehicle_list.widget.RoverListAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 @AndroidEntryPoint
 class VehicleListFragment : BaseFragment<VehicleListVM, FragmentVehicleListBinding>() {
@@ -28,18 +31,20 @@ class VehicleListFragment : BaseFragment<VehicleListVM, FragmentVehicleListBindi
         savedInstanceState: Bundle?
     ) {
         super.initUI(inflater, container, savedInstanceState)
-        viewModel.getRoverPhotos()
-
-        this.lifecycleScope.launchWhenStarted {
-            viewModel.photos.collect {
-                adapter.submitList(it)
-            }
+        with(viewModel){
+            photos.onEach(::onEachResult).launchIn(viewLifecycleOwner.lifecycleScope)
+            viewModel.getRoverPhotos()
         }
+
         binding.rvVehicleList.attach(
             adapter = adapter,
             layoutManager = linearLayoutManagerVertical,
             viewLifecycleOwner = viewLifecycleOwner
         )
+    }
+
+    private fun onEachResult(photos: List<DomainPhoto>){
+            adapter.submitList(photos)
     }
 
     private fun handleAdapterEvent(event: RoverListAdapter.Event) {
